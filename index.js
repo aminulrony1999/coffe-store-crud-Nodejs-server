5a319f04148376bfa63ee706fff93c51fb326c6f
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-02 02:59:33 +0530

    mongo db connected to the database

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
-
 //middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); //this middleware is required to access req.body
 
+
+
+const uri = "mongodb+srv://aminulrony2024:<password>@cluster0.erlhtjw.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
