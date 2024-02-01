c89e6a8150c903c029f4e525eb694c1993d9a6a4
aminulrony1999 <aminulrony1999@gmail.com>
2024-02-02 03:16:28 +0530

    username and password is being successfully hidden

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -8,8 +9,8 @@ app.use(cors());
 app.use(express.json()); //this middleware is required to access req.body
 
 
-
-const uri = "mongodb+srv://aminulrony2024:<password>@cluster0.erlhtjw.mongodb.net/?retryWrites=true&w=majority";
+//user name and password is hidded 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erlhtjw.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
